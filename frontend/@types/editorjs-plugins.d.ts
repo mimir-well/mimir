2d1bce32cfbf469b400a45e36406330a28ba0a11
sviperm <unsviperm@gmail.com>
2021-05-12 00:38:14 +0300

    Add packages and types

---
+++ frontend/@types/editorjs-plugins.d.ts
@@ -0,0 +1,10 @@
+declare module 'editorjs-paragraph-with-alignment' {
+    const plugin: any
+    export default plugin
+  }
+
+declare module 'simple-image-editorjs' {
+  const plugin: any
+  export default plugin
+}
+  
